97b629e9fadc657b443d4cb9e5a8b8b851658f8e
Amritanshu312 <ffdarkrayon@gmail.com>
2025-04-09 09:02:53 +0530

    feat 🎉: Create Link (Frontend - Home) [popup open when button is clicked]

--- components/layout/create-link/CreateLink.jsx
+++ components/layout/create-link/CreateLink.jsx
@@ -176,8 +176,6 @@ const CreateLink = () => {
       </>
     )}
   </AnimatePresence>
-
-
 }
 
 export default CreateLink
--- pages/home/no-links/CreateNewLinks.jsx
+++ pages/home/no-links/CreateNewLinks.jsx
@@ -1,7 +1,13 @@
+"use client"
 import Logo from "@/components/ui/Logo"
+import { useLink } from "@/context/linkProvider";
 import { FiLink } from "react-icons/fi";
 
+
 const CreateNewLinks = () => {
+
+  const { setIsCreateLinkPopup } = useLink()
+
   return (
     <div className="max-w-[26rem] flex flex-col items-center gap-6 mt-4">
       <div className="flex flex-col items-center justify-center gap-3">
@@ -9,7 +15,10 @@ const CreateNewLinks = () => {
         <p className="text-[#6f74a6] text-center">Start shortening your links and track their performance seamlessly. Create a new link now to simplify sharing and gain valuable insights!</p>
       </div>
 
-      <div className="bg-[linear-gradient(45deg,#201c42,#0c163f,#0f1938,#1e184b)] border border-[#1d2753] cursor-pointer rounded-md py-1.5 px-3 flex gap-3 items-center hover:bg-[linear-gradient(180deg,#201c42,#0c163f,#0f1938,#1e184b)]">
+      <div
+        className="bg-[linear-gradient(45deg,#201c42,#0c163f,#0f1938,#1e184b)] border border-[#1d2753] cursor-pointer rounded-md py-1.5 px-3 flex gap-3 items-center hover:bg-[linear-gradient(180deg,#201c42,#0c163f,#0f1938,#1e184b)]"
+        onClick={() => setIsCreateLinkPopup(prev => !prev)}
+      >
         <span><FiLink /></span>Create New Link
       </div>
     </div>
